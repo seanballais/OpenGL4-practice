2e148b855532ec6c36087ab0aabc55ec1a348c00
Sean Francis N. Ballais <sean@seanballais.com>
2016-04-27 00:07:09 +0800

    Completed the hello triangle.

--- Makefile
+++ Makefile
@@ -4,4 +4,4 @@ all:
 	make hello_triangle
 
 hello_triangle:
-	gcc hello_triangle/hello_triangle.c -o hello_triangle/hello_triangle -std=c11 -Wall -lglfw3 -lm -lGL -lGLU
+	gcc hello_triangle/hello_triangle.c -o hello_triangle/hello_triangle -std=c11 -Wall -lglfw -lGLEW -lm -lGL -lGLU
--- hello_triangle/hello_triangle.c
+++ hello_triangle/hello_triangle.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 #include <GL/glew.h>
-#include <GL/glfw.h>
+#include <GLFW/glfw3.h>
 
 int main()
 {
@@ -10,6 +10,7 @@ int main()
         return 1;
     }
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -39,6 +40,62 @@ int main()
 
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
+
+    float points[] = {
+         0.0f,  0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f
+    };
+
+    GLuint vbo = 0;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
+
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+    const char *vertexShader =
+        "#version 400\n"
+        "in vec3 vp;"
+        "void main() {"
+        "   gl_Position = vec4(vp, 1.0)"
+        "}"
+    ;
+
+    const char *fragmentShader =
+        "#version 400\n"
+        "out vec4 fragColor"
+        "void main() {"
+        "   fragColor = vec4(0.5, 0.0, 0.5, 1.0);"
+        "}"
+    ;
+
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertexShader, NULL);
+    glCompileShader(vs);
+
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragmentShader, NULL);
+    glCompileShader(fs);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, fs);
+    glAttachShader(shaderProgram, vs);
+    glLinkProgram(shaderProgram);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
 
     glfwTerminate();
     return 0;
