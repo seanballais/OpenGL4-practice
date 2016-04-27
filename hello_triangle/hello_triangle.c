fb432f6dca3b5b4cf6f7ee4693b7ddb77f17984f
Sean Francis N. Ballais <sean@seanballais.com>
2016-04-27 15:56:10 +0800

    Hello Triangle done.

--- hello_triangle/hello_triangle.c
+++ hello_triangle/hello_triangle.c
@@ -42,59 +42,56 @@ int main()
     glDepthFunc(GL_LESS);
 
     float points[] = {
-         0.0f,  0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f
+        0.0f,  0.5f,  0.0f,
+        0.5f, -0.5f,  0.0f,
+        -0.5f, -0.5f,  0.0f
     };
 
     GLuint vbo = 0;
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
+    glGenBuffers (1, &vbo);
+    glBindBuffer (GL_ARRAY_BUFFER, vbo);
+    glBufferData (GL_ARRAY_BUFFER, 9 * sizeof (float), points, GL_STATIC_DRAW);
 
     GLuint vao = 0;
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+    glGenVertexArrays (1, &vao);
+    glBindVertexArray (vao);
+    glEnableVertexAttribArray (0);
+    glBindBuffer (GL_ARRAY_BUFFER, vbo);
+    glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 
-    const char *vertexShader =
+    const char* vertex_shader =
         "#version 400\n"
         "in vec3 vp;"
-        "void main() {"
-        "   gl_Position = vec4(vp, 1.0)"
-        "}"
-    ;
+        "void main () {"
+        "  gl_Position = vec4 (vp, 1.0);"
+        "}";
 
-    const char *fragmentShader =
+    const char* fragment_shader =
         "#version 400\n"
-        "out vec4 fragColor"
-        "void main() {"
-        "   fragColor = vec4(0.5, 0.0, 0.5, 1.0);"
-        "}"
-    ;
+        "out vec4 frag_colour;"
+        "void main () {"
+        "  frag_colour = vec4 (0.5, 0.0, 0.5, 1.0);"
+        "}";
 
-    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vs, 1, &vertexShader, NULL);
-    glCompileShader(vs);
+    GLuint vs = glCreateShader (GL_VERTEX_SHADER);
+    glShaderSource (vs, 1, &vertex_shader, NULL);
+    glCompileShader (vs);
+    GLuint fs = glCreateShader (GL_FRAGMENT_SHADER);
+    glShaderSource (fs, 1, &fragment_shader, NULL);
+    glCompileShader (fs);
 
-    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fs, 1, &fragmentShader, NULL);
-    glCompileShader(fs);
+    GLuint shader_programme = glCreateProgram ();
+    glAttachShader (shader_programme, fs);
+    glAttachShader (shader_programme, vs);
+    glLinkProgram (shader_programme);
 
-    GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, fs);
-    glAttachShader(shaderProgram, vs);
-    glLinkProgram(shaderProgram);
-
-    while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glUseProgram(shaderProgram);
-        glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        glfwPollEvents();
-        glfwSwapBuffers(window);
+    while (!glfwWindowShouldClose (window)) {
+        glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram (shader_programme);
+        glBindVertexArray (vao);
+        glDrawArrays (GL_TRIANGLES, 0, 3);
+        glfwPollEvents ();
+        glfwSwapBuffers (window);
     }
 
     glfwTerminate();
